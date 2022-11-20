640ac1ffafcc4b8cd013c2d679b0d98222e34998
Ruben-Varghese007 <116579510+Ruben-Varghese007@users.noreply.github.com>
2022-11-20 20:02:10 +0530

    main.py
    Convert html division to pdf using python.

---
+++ main.py
@@ -0,0 +1,13 @@
+import pdfkit
+
+#Path to wkhtmltopdf.exe
+path_to_wkhtmltopdf = r'D:\Internship\wkhtmltopdf\bin\wkhtmltopdf.exe'
+
+#Path to HTML file
+path_to_file = 'sample.html'
+
+#Point pdfkit configuration to wkhtmltopdf.exe
+config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
+
+#Convert HTML file to PDF
+pdfkit.from_file(path_to_file, output_path='sample.pdf', configuration=config)
